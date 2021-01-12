e651215ba9b6842ceff64f7ac81e757b607682b7
Bielo López Lauber <38672453+bielolopez@users.noreply.github.com>
2021-01-12 16:48:11 -0300

    Update tienda_inglesa.py

--- tienda_inglesa.py
+++ tienda_inglesa.py
@@ -16,7 +16,7 @@ col = db['alimentos']
 
 # Definimos el User Agent en Selenium utilizando la clase Options
 opts = Options()
-opts.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36")
+opts.add_argument("user-agent=...")
 driver = webdriver.Chrome('./chromedriver.exe', options=opts) # REMPLAZA AQUI EL NOMBRE DE TU CHROME DRIVER
 
 #URL SEMILLA
